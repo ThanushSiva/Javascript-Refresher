a8809ca361749b364caad494a77fb169e94c2bdb
thanush <thanushsiva06@gmail.com>
2022-06-20 13:22:16 +0530

    Added Arrow Functions

---
+++ Arrow Functions/new.js
@@ -0,0 +1,5 @@
+// new function
+
+const myFunction = (z) => z*z
+const a = myFunction(2)
+console.log(a);
---
+++ Arrow Functions/old.js
@@ -0,0 +1,8 @@
+// old function
+
+function myFunction(z){
+    return z*z
+}
+
+const a=myFunction(2)
+console.log(a);
